47d0ac137c785d42dffad9a7788440069fe060d6
Sam Hoang <samhv.ict@gmail.com>
2017-05-18 19:22:01 +0700

    + login / logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new', as: 'signup'
-  post '/signup', to: 'users#create', as: 'users'
+  get 'sessions/new'
+
+  get '/signup', to: 'users#new', as: 'get_signup'
+  post '/signup', to: 'users#create', as: 'post_signup'
+
+  get    '/login',   to: 'sessions#new', as: 'get_login'
+  post   '/login',   to: 'sessions#create', as: 'post_login'
+  delete '/logout',  to: 'sessions#destroy', as: 'logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home/index', as: 'home'
   get 'book/manage'
--- db/schema.rb
+++ db/schema.rb
@@ -12,20 +12,20 @@
 
 ActiveRecord::Schema.define(version: 20170518013234) do
 
-  create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
     t.string   "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "authors_books", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "authors_books", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer "author_id", null: false
     t.integer "book_id",   null: false
     t.index ["book_id", "author_id"], name: "index_authors_books_on_book_id_and_author_id", unique: true, using: :btree
   end
 
-  create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "title"
     t.decimal  "price",      precision: 10, scale: 2
     t.integer  "qty"
@@ -33,20 +33,20 @@ ActiveRecord::Schema.define(version: 20170518013234) do
     t.datetime "updated_at",                          null: false
   end
 
-  create_table "books_categories", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "books_categories", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer "category_id", null: false
     t.integer "book_id",     null: false
     t.index ["book_id", "category_id"], name: "index_books_categories_on_book_id_and_category_id", unique: true, using: :btree
   end
 
-  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "parent_id",  default: -1
     t.string   "title"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
 
-  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "user_id"
     t.integer  "book_id"
     t.string   "comment"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20170518013234) do
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
-  create_table "order_books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "order_books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "order_id"
     t.integer  "book_id"
     t.integer  "qty"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 20170518013234) do
     t.index ["order_id"], name: "index_order_books_on_order_id", using: :btree
   end
 
-  create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "user_id"
     t.boolean  "is_checkout", default: false
     t.datetime "created_at",                  null: false
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 20170518013234) do
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
 
-  create_table "reviews", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "reviews", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "user_id"
     t.integer  "book_id"
     t.string   "review"
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 20170518013234) do
     t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
   end
 
-  create_table "subscribes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "subscribes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "user_id"
     t.integer  "book_id"
     t.boolean  "availabe",   default: false
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 20170518013234) do
     t.index ["user_id"], name: "index_subscribes_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
     t.string   "email"
     t.boolean  "is_admin",        default: false
