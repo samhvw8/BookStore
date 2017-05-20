20fad4fb27f55a5fc160106e2f724596d560b23c
Sam Hoang <samhv.ict@gmail.com>
2017-05-20 23:15:12 +0700

    + basic function for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
     @user = User.new
@@ -9,13 +15,31 @@ class UsersController < ApplicationController
     @user = User.new(user_params) # Not the final implementation!
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      login(@user)
-      redirect_to home_path
+      log_in(@user)
+      redirect_to root_path
     else
       render 'new'
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      render 'show'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
   private
 
@@ -24,4 +48,20 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+
+  def admin_user
+    redirect_to(root_url) unless current_user.is_admin?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
+  get '/profile', to: 'sessions#show', as: 'profile'
   get '/signup', to: 'users#new', as: 'get_signup'
   post '/signup', to: 'users#create', as: 'post_signup'
 
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create', as: 'post_login'
   delete '/logout',  to: 'sessions#destroy', as: 'logout'
 
+  resources :users, except: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'home/index', as: 'home'
-  get 'book/manage'
+  get '/', to: 'home#index', as: 'root'
+
 end
