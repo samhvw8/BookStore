16559d5c9498c40459a70aa81571a45f2d9c4cc8
LuongNV <luongnv@ows.edu.vn>
2017-05-24 10:48:07 +0700

    Add: create a new book

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,6 +1,11 @@
 class BookController < ApplicationController
 	layout 'application'
     def manage
-        
+        @authors = Author.all
+        @category = Category.all
+    end
+
+    def create
+    	
     end
 end
