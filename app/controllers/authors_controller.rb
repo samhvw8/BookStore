ad9d5f0eeec580c0b2aeb8b32caa0314f6a0831d
LuongNV <luongnv@ows.edu.vn>
2017-05-25 10:55:31 +0700

    Add: add author

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,16 @@
+class AuthorsController < ApplicationController
+  def new
+    @author = Author.new
+  end
+
+  def create
+    author_params = params.require(:author).permit(:name, :bio)
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to book_new_path
+    else
+      render 'new'
+    end
+  end
+end
