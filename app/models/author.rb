0c5cefb28b56208bda625f0ee47a1b449f5ebd26
Sam Hoang <samhv.ict@gmail.com>
2017-05-29 23:42:32 +0700

    fix model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,15 +1,17 @@
 class Author < ApplicationRecord
 
   has_many :author_readings
+  has_many :comics, :through => :author_readings, :source => :reading, :source_type => 'Comic'
+  has_many :novels, :through => :author_readings, :source => :reading, :source_type => 'Novel'
 
   validates :name, presence: true, allow_blank: false, uniqueness: true
   validates :bio, presence: true
 
-  def readings
-    author_reading.map(&:reading)
+  def add_comic(comic)
+    comics << comic unless comics.include?(comic)
   end
 
-  def add_book(reading)
-    readings << reading unless readings.include?(reading)
+  def add_novel(novel)
+    novels << novel unless novels.include?(novel)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :book
+  belongs_to :reading, polymorphic: true
   validates :review, presence: true, allow_blank: false
   validates_numericality_of :vote, presence: true, numericality:{:greater_than_or_equal_to => 0}
 end
