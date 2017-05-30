c264a5bf8c5e4f005513b35ae5b804b3fc19ea53
Sam Hoang <samhv.ict@gmail.com>
2017-05-30 16:48:51 +0700

    update view, controler

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'order_book/:id/:qty', to: 'order_books#update', as: 'order_books'
-  delete 'order_book/:id', to: 'order_books#destroy', as: 'delete_order_books'
 
-  get 'carts', to: 'carts#show', as: 'carts'
+  resources :novels, only: [:index, :show ]
 
-  delete 'carts/destroy', to: 'carts#destroy', as: 'delete_carts'
-
-  put 'books/:id/order', to: 'carts#update', as: 'add_to_cart'
-  put 'books/:id/order/:order_book', to: 'order_books#update', as: 'update_orderbook'
-  delete 'books/:id/order/:order_book', to: 'order_books#delete', as: 'delete_orderbook'
+  resources :comics, only: [:index, :show ]
 
   get 'profile', to: 'sessions#show', as: 'profile'
   get 'signup', to: 'users#new', as: 'get_signup'
@@ -21,19 +15,13 @@ Rails.application.routes.draw do
 
   resources :users, except: [:index, :show]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get '/', to: 'home#index', as: 'root'
 
 
-  # Book management routes
+  resources :chapters, only: [:show]
 
-
-  get 'manage/books/', to: 'books#manage', as: 'books_management'
-
-  resources :books, only: [:show, :new, :create]
-
-
-  resources :categories
+  resources :categories, only: [:show, :index]
 
   resources :authors, only: [:show, :new, :create]
 
